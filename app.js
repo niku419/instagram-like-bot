bf22c52b5fa8fa796a228dbae02bbbd7d9c9d733
Niku_419 <2019198@iiitdmj.ac.in>
2021-06-03 12:57:38 +0530

    :rocket: my first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  try{
+    const browser = await puppeteer.launch({headless:false, slowMo:10});
+  const page = await browser.newPage();
+  await page.goto('https://www.instagram.com',{waitUntil :"networkidle2"});
+  await page.waitFor(1000)
+  await page.type('input[name="username"]',"supernicky659@gmail.com",{delay:500})
+  await page.type('input[name="password"]',"9438002199",{delay:500})
+  await page.waitForSelector('form>div>div:nth-child(3)>button')
+  await page.click('form>div>div:nth-child(3)>button')
+  await page.waitFor(10000)
+  var searchUser = "maakasamdilipjoshi"
+  await page.goto('https://www.instagram.com/'+searchUser,{waitUntil:"networkidle2"})
+  await page.waitFor(10000)
+  try{
+    var stories = await page.$$eval('article>div>div>div>div>a', anchors => [].map.call(anchors, a => a.href))
+    console.log(stories)
+    for(let i=0;i<stories.length;i++){
+      await page.goto(stories[i],{waitUntil: "networkidle2"})
+      let isLikable= await page.$eval('section>span>button[type="button"]>div>span>svg',el => el.getAttribute("aria-label"))
+      console.log(isLikable)
+      if(isLikable == "Like"){
+        await page.click('section>span>button')
+      }
+      await page.waitFor(3000)
+    }
+  }
+  catch(error){
+    throw "Some error occured"
+  }
+  
+
+  await page.waitFor(5000)
+  }catch(err){
+    throw(err)
+  }
+  
+})();
